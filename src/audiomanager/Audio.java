9479c97eaeaeb6e43962f6894f730428dcf3eec6
Joseph BRIGUET <jbriguet@s-id.local>
2021-12-17 16:54:28 +0100

    :one: v1.0 - First commit

---
+++ src/audiomanager/Audio.java
@@ -0,0 +1,331 @@
+/*
+ * Copyright (C) JasonPercus Systems, Inc - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by JasonPercus, December 2021
+ */
+package audiomanager;
+
+
+
+/**
+ * This class manages the main windows volume
+ * @author JasonPercus
+ * @version 1.0
+ */
+@SuppressWarnings({"SleepWhileInLoop", "SleepWhileHoldingLock"})
+public final class Audio {
+    
+    
+    
+//ATTRIBUTS STATICS
+    /**
+     * Determines whether or not the class has been initialized
+     */
+    public static boolean inited = false;
+    
+    /**
+     * Corresponds to the manager who should be informed of the results
+     */
+    public static MyManager manager;
+    
+    /**
+     * Corresponds to the id of the speaker windows
+     */
+    private static String audioID;
+    
+    /**
+     * Determines if the speaker windows is muted
+     */
+    private static boolean muted;
+    
+    /**
+     * Corresponds to the value of the windows volume
+     */
+    private static int volumeValue;
+    
+    /**
+     * Corresponds to a thread which will regularly analyze the value of the windows volume as well as its muted value
+     */
+    private static Analyser analyser = null;
+    
+    /**
+     * Determines whether the scanning process is in progress or not
+     */
+    private static boolean analyse;
+    
+    
+    
+//METHODES PUBLICS STATICS
+    /**
+     * Initializes the class
+     */
+    public static void init(){
+        if(!inited){
+            audioID = id();
+            analyse = true;
+            startAnalyser();
+        }
+    }
+    
+    /**
+     * Stop analysis
+     */
+    public synchronized static void stop(){
+        analyse = false;
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            manager.log(ex);
+        }
+    }
+    
+    /**
+     * Returns whether or not the windows sound is muted or not
+     * @return Returns whether or not the windows sound is muted or not
+     */
+    public synchronized static boolean isMute(){
+        return muted = isMute(audioID);
+    }
+    
+    /**
+     * Mute or restore windows sound
+     * @return Returns true if the sound is muted, otherwise false
+     */
+    public synchronized static boolean toggleMute(){
+        if(isMute()){
+            unmute(audioID);
+            return false;
+        }else{
+            mute(audioID);
+            return true;
+        }
+    }
+    
+    /**
+     * Returns the value of the windows volume [0, 100]
+     * @return Returns the value of the windows volume
+     */
+    public synchronized static int getVolume(){
+        return volumeValue = (int) getVolume(audioID);
+    }
+    
+    /**
+     * Increase windows volume
+     * @param value Corresponds to the new value of the windows sound [0, 100]
+     * @return Returns the new value of the windows sound
+     */
+    public synchronized static int increase(int value){
+        int volume = (int) getVolume(audioID);
+        volume += value;
+        if(volume > 100) volume = 100;
+        
+        setVolume(audioID, volume);
+        
+        return volume;
+    }
+    
+    /**
+     * Decrease windows volume
+     * @param value Corresponds to the new value of the windows sound [0, 100]
+     * @return Returns the new value of the windows sound
+     */
+    public synchronized static int discrease(int value){
+        int volume = (int) getVolume(audioID);
+        volume -= value;
+        if(volume < 0) volume = 0;
+        
+        setVolume(audioID, volume);
+        
+        return volume;
+    }
+    
+    
+    
+//METHODES PRIVATES STATICS
+    /**
+     * Starts the analyzer will regularly analyze the value of the windows volume as well as its muted value
+     */
+    private synchronized static void startAnalyser(){
+        if(analyser == null){
+            analyser = new Analyser();
+            analyser.start();
+        }
+    }
+    
+    /**
+     * Returns the id of the windows speaker
+     * @return Returns the id of the windows speaker
+     */
+    private synchronized static String id(){
+        String line = null;
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "Audio.bat");
+            Process p = pb.start();
+            try (java.io.BufferedWriter bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()))) {
+                bw.write("1");
+                bw.flush();
+                line=br.readLine();
+            }
+        } catch (java.io.IOException ex) {}
+        return line;
+    }
+    
+    /**
+     * Returns whether or not the windows sound is muted or not
+     * @param id Corresponds to the id of the speaker windows
+     * @return Returns whether or not the windows sound is muted or not
+     */
+    private synchronized static boolean isMute(String id){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "Audio.bat");
+            Process p = pb.start();
+            try (java.io.BufferedWriter bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()))) {
+                bw.write("2");
+                bw.flush();
+                br.readLine();
+                bw.write(id);
+                bw.flush();
+                return br.readLine().equals("1");
+            }
+        } catch (java.io.IOException ex) {
+            manager.log(ex);
+        }
+        return false;
+    }
+    
+    /**
+     * Returns the value of the windows volume [0, 100]
+     * @param id Corresponds to the id of the speaker windows
+     * @return Returns the value of the windows volume
+     */
+    private synchronized static float getVolume(String id){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "Audio.bat");
+            Process p = pb.start();
+            try (java.io.BufferedWriter bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()))) {
+                bw.write("3");
+                bw.flush();
+                br.readLine();
+                bw.write(id);
+                bw.flush();
+                return Float.parseFloat(br.readLine());
+            }
+        } catch (java.io.IOException ex) {
+            manager.log(ex);
+        }
+        return -1f;
+    }
+    
+    /**
+     * Mutes main windows sound
+     * @param id Corresponds to the id of the speaker windows
+     */
+    private synchronized static void mute(String id){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "Audio.bat");
+            Process p = pb.start();
+            try (java.io.BufferedWriter bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()))) {
+                bw.write("4");
+                bw.flush();
+                br.readLine();
+                bw.write(id);
+                bw.flush();
+            }
+        } catch (java.io.IOException ex) {
+            manager.log(ex);
+        }
+    }
+    
+    /**
+     * Unmutes main windows sound
+     * @param id Corresponds to the id of the speaker windows
+     */
+    private synchronized static void unmute(String id){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "Audio.bat");
+            Process p = pb.start();
+            try (java.io.BufferedWriter bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()))) {
+                bw.write("5");
+                bw.flush();
+                br.readLine();
+                bw.write(id);
+                bw.flush();
+            }
+        } catch (java.io.IOException ex) {
+            manager.log(ex);
+        }
+    }
+    
+    /**
+     * Modifies the value of the main sound of windows
+     * @param id Corresponds to the id of the speaker windows
+     * @param volume Corresponds to the new sound value [0, 100]
+     */
+    private synchronized static void setVolume(String id, float volume){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "Audio.bat");
+            Process p = pb.start();
+            try (java.io.BufferedWriter bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()))) {
+                bw.write("6");
+                bw.flush();
+                br.readLine();
+                bw.write(id);
+                bw.flush();
+                br.readLine();
+                bw.write(""+((int)volume));
+                bw.flush();
+            }
+        } catch (java.io.IOException ex) {
+            manager.log(ex);
+        }
+    }
+    
+    
+    
+//CLASS STATIC
+    /**
+     * This static class represents the windows sound analyzer
+     * @author JasonPercus
+     * @version 1.0
+     */
+    private static class Analyser extends Thread {
+
+        
+        
+        /**
+         * Starts analysis
+         */
+        @Override
+        public void run() {
+            while(analyse){
+                try {
+                    Thread.sleep(1000);
+                    
+                    boolean res = isMute(audioID);
+                    if(res != muted){
+                        muted = res;
+                        manager.muteChangedListener(res);
+                    }
+                    
+                    int val = (int) getVolume(audioID);
+                    if(val != volumeValue){
+                        volumeValue = val;
+                        manager.volumeChangedListener(val);
+                    }
+                    
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    
+}
---
+++ src/audiomanager/MyManager.java
@@ -0,0 +1,282 @@
+/*
+ * Copyright (C) JasonPercus Systems, Inc - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by JasonPercus, December 2021
+ */
+package audiomanager;
+
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.jasonpercus.plugincreator.models.Context;
+import com.jasonpercus.plugincreator.models.Extension;
+import com.jasonpercus.plugincreator.models.Target;
+import com.jasonpercus.plugincreator.models.events.KeyUp;
+import com.jasonpercus.plugincreator.models.events.WillAppear;
+import com.jasonpercus.plugincreator.models.events.WillDisappear;
+
+
+
+/**
+ * Allows control of sound tiles
+ * @author JasonPercus
+ * @version 1.0
+ */
+@SuppressWarnings({"LeakingThisInConstructor", "NestedSynchronizedStatement"})
+public final class MyManager extends com.jasonpercus.plugincreator.EventManager {
+
+    
+    
+//ATTRIBUTS
+    /**
+     * Corresponds to the mute image
+     */
+    private byte[] muteImg;
+    
+    /**
+     * Corresponds to the unmute image
+     */
+    private byte[] unmuteImg;
+    
+    /**
+     * Corresponds to the list of "mute" action contexts
+     */
+    private final java.util.List<Context> MUTE_ACTIONS = new java.util.ArrayList<>();
+    
+    /**
+     * Corresponds to the full list of action contexts
+     */
+    private final java.util.List<Context> All_ACTIONS = new java.util.ArrayList<>();
+    
+
+    
+//CONSTRUCTOR
+    /**
+     * Create a sound tiles manager
+     */
+    public MyManager() {
+        try {
+            this.muteImg = java.nio.file.Files.readAllBytes(new java.io.File("images/mute.png").toPath());
+        } catch (java.io.IOException ex) {
+        }
+        try {
+            this.unmuteImg = java.nio.file.Files.readAllBytes(new java.io.File("images/unmute.png").toPath());
+        } catch (java.io.IOException ex) {
+        }
+    }
+    
+    
+    
+//ON
+    /**
+     * When the EventManager has been created
+     */
+    @Override
+    public void onCreate() {
+        Audio.manager = this;
+        Audio.init();
+    }
+    
+    /**
+     * When the EventManager is destroyed. This happens before the app is closed.
+     */
+    @Override
+    public void onDestroy() {
+        Audio.stop();
+    }
+    
+    
+    
+//EVENTS
+    /**
+     * When the user releases a key, the plugin will receive the keyUp event
+     * @param event Corresponds to the Stream Deck event
+     * @param context Corresponds to the context (or ID) of the action
+     * @param builder Allows to deserialize the received json
+     */
+    @Override
+    public void keyUp(KeyUp event, Context context, GsonBuilder builder) {
+        if(event.action.equals("discrease")){
+            int value = getValue(event.payload.settings, builder);
+            value = Audio.discrease(value);
+            synchronized(All_ACTIONS){
+                for(Context c : All_ACTIONS)
+                    setTitle(c, value + "%", Target.HARDWARE);
+            }
+        }
+        if(event.action.equals("increase")){
+            int value = getValue(event.payload.settings, builder);
+            value = Audio.increase(value);
+            synchronized(All_ACTIONS){
+                for(Context c : All_ACTIONS)
+                    setTitle(c, value + "%", Target.HARDWARE);
+            }
+        }
+        if(event.action.equals("mute")){
+            if(Audio.toggleMute()){
+                //il est mute
+                synchronized(MUTE_ACTIONS){
+                    for(Context c : MUTE_ACTIONS)
+                        setImage(c, muteImg, Extension.PNG, Target.BOTH);
+                }
+            }else{
+                //il n'est pas mute
+                synchronized(MUTE_ACTIONS){
+                    for(Context c : MUTE_ACTIONS)
+                        setImage(c, unmuteImg, Extension.PNG, Target.BOTH);
+                }
+            }
+        }
+    }
+
+    /**
+     * When an instance of an action is displayed on the Stream Deck, for example when the hardware is first plugged in, or when a folder containing that action is entered, the plugin will receive a willAppear event
+     * @param event Corresponds to the Stream Deck event
+     * @param context Corresponds to the context (or ID) of the action
+     * @param builder Allows to deserialize the received json
+     */
+    @Override
+    public void willAppear(WillAppear event, Context context, GsonBuilder builder) {
+        synchronized(MUTE_ACTIONS){
+            if(event.action.equals("mute")){
+                if(!MUTE_ACTIONS.contains(context))
+                    MUTE_ACTIONS.add(context);
+            }
+        }
+        synchronized(All_ACTIONS){
+            if(event.action.equals("mute") || event.action.equals("increase") || event.action.equals("discrease")){
+                if(!All_ACTIONS.contains(context))
+                    All_ACTIONS.add(context);
+            }
+        }
+        if(event.action.equals("mute")){
+            if(Audio.isMute()){
+                //il est mute
+                setImage(context, muteImg, Extension.PNG, Target.BOTH);
+            }else{
+                //il n'est pas mute
+                setImage(context, unmuteImg, Extension.PNG, Target.BOTH);
+            }
+            setTitle(context, Audio.getVolume() + "%", Target.HARDWARE);
+        }
+        if(event.action.equals("increase")){
+            setTitle(context, Audio.getVolume() + "%", Target.HARDWARE);
+        }
+        if(event.action.equals("discrease")){
+            setTitle(context, Audio.getVolume() + "%", Target.HARDWARE);
+        }
+    }
+
+    /**
+     * When an instance of an action ceases to be displayed on Stream Deck, for example when switching profiles or folders, the plugin will receive a willDisappear event
+     * @param event Corresponds to the Stream Deck event
+     * @param context Corresponds to the context (or ID) of the action
+     * @param builder Allows to deserialize the received json
+     */
+    @Override
+    public void willDisappear(WillDisappear event, Context context, GsonBuilder builder) {
+        synchronized(MUTE_ACTIONS){
+            if(event.action.equals("mute")){
+                if(MUTE_ACTIONS.contains(context))
+                    MUTE_ACTIONS.remove(context);
+            }
+        }
+        synchronized(All_ACTIONS){
+            if(event.action.equals("mute") || event.action.equals("increase") || event.action.equals("discrease")){
+                if(All_ACTIONS.contains(context))
+                    All_ACTIONS.remove(context);
+            }
+        }
+    }
+    
+    
+    
+//METHODES PUBLICS
+    /**
+     * When the sound is muted or not from the computer
+     * @param isMute Determines whether the sound is muted
+     */
+    public synchronized void muteChangedListener(boolean isMute){
+        synchronized(MUTE_ACTIONS){
+            if(isMute){
+                for(Context context : MUTE_ACTIONS){
+                    setImage(context, muteImg, Extension.PNG, Target.BOTH);
+                }
+            }else{
+                for(Context context : MUTE_ACTIONS){
+                    setImage(context, unmuteImg, Extension.PNG, Target.BOTH);
+                }
+            }
+        }
+    }
+    
+    /**
+     * When the sound volume has changed from the computer
+     * @param value Corresponds to the new percentage of sound
+     */
+    public synchronized void volumeChangedListener(int value){
+        synchronized(All_ACTIONS){
+            for(Context context : All_ACTIONS){
+                setTitle(context, value + "%", Target.HARDWARE);
+            }
+        }
+    }
+    
+    
+    
+//METHODE PRIVATE
+    /**
+     * Returns the value included in the settings at the click of a button
+     * @param json Corresponds to settings
+     * @param builder Allows to deserialize the received json
+     * @return Allows to deserialize the received json
+     */
+    private int getValue(String json, GsonBuilder builder){
+        Gson gson = builder.registerTypeAdapter(String.class, new ValueDeserializer()).create();
+        String value = gson.fromJson(json, String.class);
+        if(value != null)
+            return Integer.parseInt(value);
+        return -1;
+    }
+    
+    
+    
+//CLASS
+    /**
+     * Allows to deserialize the received json
+     * @author JasonPercus
+     * @version 1.0
+     */
+    private class ValueDeserializer implements JsonDeserializer<String>{
+
+        @Override
+        public String deserialize(JsonElement je, java.lang.reflect.Type type, JsonDeserializationContext jdc) throws JsonParseException {
+            JsonObject obj = je.getAsJsonObject();
+
+            JsonPrimitive increase  = obj.getAsJsonPrimitive("increaseValue");
+            JsonPrimitive discrease = obj.getAsJsonPrimitive("discreaseValue");
+            
+            if(increase != null && discrease == null)
+                return increase.getAsString();
+            
+            if(increase == null && discrease != null)
+                return discrease.getAsString();
+            
+            return "5";
+        }
+        
+    }
+    
+    
+    
+}
